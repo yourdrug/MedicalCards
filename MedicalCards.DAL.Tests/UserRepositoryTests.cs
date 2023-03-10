2466506ba7458ac828e88637a6f055c3904e3dc3
Ivan Rozanov <navi.zor9@gmail.com>
2023-03-10 23:23:58 +0300

    add UserRepositoryTests example

---
+++ MedicalCards.DAL.Tests/UserRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using MedicalCards.DAL.Entities;
+using MedicalCards.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCards.DAL.Tests
+{
+    public class UserRepositoryTests : RepositoryTests<User>
+    {
+        public UserRepositoryTests() : base(context => new UserRepository(context))
+        {
+            repository = new UserRepository(_context);
+        }
+    }
+}
