99a03e291ce4cf73ab9879421b226c0908b67ab3
Ivan Rozanov <navi.zor9@gmail.com>
2023-03-10 23:23:36 +0300

    add Generic Repository Tests  template

---
+++ MedicalCards.DAL.Tests/RepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using MedicalCards.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCards.DAL.Tests
+{
+    public abstract class RepositoryTests<TEntity> : IDisposable
+        where TEntity : class
+    {
+        protected readonly DbContext _context  = new AppContext();
+        protected Repository<TEntity> repository;
+        protected RepositoryTests(Func<DbContext, Repository<TEntity>> createReporitory)
+        {
+            _context.Database.EnsureCreated();
+            repository = createReporitory(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        [Fact]
+        public async Task GetAll_EntitiesDoNotExist_ReturnsEmptyList()
+        {
+            var actual = await repository.GetAll();
+
+            actual.Should().BeEmpty();
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+    }
+}
