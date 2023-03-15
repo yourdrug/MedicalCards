a25057413fb8838a8035a77306d10c935b221aa1
Женя Митько <mitkojenia@gmail.com>
2023-03-15 21:56:24 +0300

    Add Registration Form

--- WindowAuth/Properties/AssemblyInfo.cs
+++ WindowAuth/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WindowAuth")]
+[assembly: AssemblyTitle("AuthWindow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowAuth")]
+[assembly: AssemblyProduct("AuthWindow")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- WindowAuth/Properties/Resources.Designer.cs
+++ WindowAuth/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowAuth.Properties
+namespace AuthWindow.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WindowAuth.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowAuth.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AuthWindow.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
