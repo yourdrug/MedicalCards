9d3109d0822cc15a521ab664254f5ee1ebfb38e4
Ivan Rozanov <navi.zor9@gmail.com>
2023-03-18 15:53:11 +0300

    add user service example

---
+++ MedicalCards.BLL/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using MedicalCards.DAL;
+using MedicalCards.DAL.Entities;
+using MedicalCards.DAL.Repositories;
+using MedicalCards.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalCards.BLL.Services
+{
+    public class UserService : IDisposable
+    {
+        private readonly IUserRepository userRepository;
+        public UserService(IUserRepository userRepository)
+        { 
+            this.userRepository = userRepository;
+        }
+
+        public async Task<User> Authentificate(string login, string password)
+        {
+            string hash = string.Empty;
+            //hash = password.Hash();
+
+            var user = await userRepository.FindByCredits(login, hash);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            userRepository.Dispose();
+        }
+    }
+}
