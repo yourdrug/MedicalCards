b4df2f52b1f6030b34a520094cc5e22b1fc1b9b3
Женя Митько <mitkojenia@gmail.com>
2023-03-19 17:44:37 +0300

    Add Access at User

--- MedicalCards.DAL/Migrations/20230319113537_Add Access at User.Designer.cs
+++ MedicalCards.DAL/Migrations/20230319113537_Add Access at User.Designer.cs
@@ -4,6 +4,7 @@ using MedicalCards.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicalCards.DAL.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20230319113537_Add Access at User")]
+    partial class AddAccessatUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
